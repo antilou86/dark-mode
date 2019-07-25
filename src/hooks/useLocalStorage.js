04f855acc27f2dd14b9759641953bd6e092c7752
Lou <antilou86@gmail.com>
2019-07-25 15:51:48 -0400

    WIP: added setter to useLocalStorage custom hook. this allows us to define the storedValue variable, beyond the initial defined value, and store the value as a string in local storage. part 1 completed.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,6 +5,11 @@ function useLocalStorage(key, initialValue) {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     })
+    const setValue = (value) => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+    return [storedValue, setStoredValue];
 }
 
 export default useLocalStorage
